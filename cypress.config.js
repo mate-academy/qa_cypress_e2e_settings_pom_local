069abbb130e5fc5e857f1f59232b408116e843ef
Yaroslav Khoma <yaroamazone@gmail.com>
2023-06-08 16:32:37 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -9,11 +9,12 @@ module.exports = defineConfig({
       on("task", {
         generateUser() {
           let randomNumber = Math.ceil(Math.random(1000) * 1000);
-          let userName = faker.name.firstName() + `${randomNumber}`
+          let userName = faker.name.firstName() + `${randomNumber}`;
           return {
             username: userName.toLowerCase(),
             email: 'test'+`${randomNumber}`+'@mail.com',
-            password: '12345Qwert!',
+            password: 'Qwert12345!',
+            bio: faker.random.words(),
           };
         },
         generateArticle() {
