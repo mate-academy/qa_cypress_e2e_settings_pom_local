d1602ed4c41e4ded648db625fe20b4f556830fe9
Svitlana Zai <svetkaqq1991@gmail.com>
2023-09-27 18:10:44 +0300

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,72 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
+import SettingsPage from '../support/pages/settings.PageObject';
+import HomePageObject from '../support/pages/home.pageObject';
+import SignInPage from '../support/pages/signIn.pageObject';
+import faker from 'faker';
+
+const settingsPage = new SettingsPage();
+const header = new HomePageObject();
+const loginAfterUpdate = new SignInPage();
 
 describe('Settings page', () => {
-  before(() => {
-
-  });
-
+  let user;
+  let updatedUser;
+  
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    cy.login(user.email, user.username, user.password);
+    });
+    cy.task('generateUser').then((generateUser) => {
+      updatedUser = generateUser;
+    });
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.visit();
+    settingsPage.fillField('username-field', updatedUser.username);
+    settingsPage.clickOnUpdateSettingBtn();
+    settingsPage.confirmUpdatedLink(updatedUser.username);
+    header.assertHeaderContainUsername(updatedUser.username);
+    settingsPage.assertUpdatedInfo(updatedUser.username);
   });
 
   it('should provide an ability to update bio', () => {
-
+    
+    const updatedBio = faker.lorem.words();
+    settingsPage.visit();
+    settingsPage.fillField('bio-field', updatedBio);
+    settingsPage.clickOnUpdateSettingBtn();
+    settingsPage.assertUpdatedInfo(updatedBio);
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.visit();
+    settingsPage.fillField('email-field', updatedUser.email);
+    settingsPage.clickOnUpdateSettingBtn();
+    loginAfterUpdate.visit();
+    loginAfterUpdate.typeEmail(updatedUser.email);
+    loginAfterUpdate.typePassword(user.password);
+    loginAfterUpdate.clickSignInBtn();
   });
 
   it('should provide an ability to update password', () => {
-
+    const updatedPassword = 'Pas123!';
+    
+    settingsPage.visit();
+    settingsPage.fillField('password-field', updatedPassword);
+    settingsPage.clickOnUpdateSettingBtn();
+    loginAfterUpdate.visit();
+    loginAfterUpdate.typeEmail(user.email);
+    loginAfterUpdate.typePassword(updatedPassword);
+    loginAfterUpdate.clickSignInBtn();
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.visit();
+    settingsPage.clickOnLogoutBtn();
+    cy.get('.nav-link').should('contain', 'Sign in');
   });
 });
