6bbd24f72ef47ee022fa6d8276ef2e590b5b652b
Yevhen Boiko <angry90.jb@gmail.com>
2023-11-14 20:10:19 +0200

    Settings

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -11,6 +11,24 @@ class HomePageObject extends PageObject {
     this.usernameLink
       .should('contain', username);
   }
+
+  assertUserAccount(username) {
+    cy.getByDataCy('profile-link')
+      .should ('contain', username);
+  }
+
+  clickOnSignIn() {
+    cy.contains('a', 'Sign in').click();
+  }
+
+  fillEmailField(email) {
+    cy.getByDataCy('email-sign-in').type(email);
+  }
+
+  fillPasswordField(password) {
+    cy.getByDataCy('password-sign-in').type(password);
+  }
+
 }
 
 export default HomePageObject;
