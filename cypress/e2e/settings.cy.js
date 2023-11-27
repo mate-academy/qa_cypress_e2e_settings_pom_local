2cde72c41035e6a97cb7674fa6621ae082b7ff3b
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-11-27 16:02:30 +0200

    solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,71 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+import homePageObject from '../support/pages/home.pageObject';
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject';
 
-  });
+import faker from 'faker';
+
+const settingsPage = new SettingsPageObject();
+const homePage = new homePageObject();
+const signInPage = new SignInPageObject();
+
+
+describe('Settings page', () => {
+  let user;
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.login(user.email, user.username, user.password);   
+      cy.visit('/settings');    
+    });
   });
 
-  it('should provide an ability to update username', () => {
+    const newUser = {
+      newUserName: faker.name.firstName().toLowerCase(),
+      newBio: faker.lorem.words(),
+      newEmail: faker.internet.email().toLowerCase(),
+      newPassword: 'Qwert12345!'
+    };
 
+    it('should provide an ability to update username', () => { 
+      settingsPage.typeUsername(newUser.newUserName);
+      settingsPage.clickUpdateBtn();
+      homePage.assertHeaderContainUsername(newUser.newUserName);
+    });
+
+    it('should provide an ability to update bio', () => {
+      settingsPage.typeBio(newUser.newBio);
+      settingsPage.clickUpdateBtn();
+      cy.visit('/settings'); 
+      settingsPage.assertBioContainValue(newUser.newBio);
+    });
+
+    it('should provide an ability to update an email', () => {
+      settingsPage.typeEmail(newUser.newEmail);
+      settingsPage.clickUpdateBtn();
+      cy.visit('/settings');
+      settingsPage.assertEmailContainValue(newUser.newEmail);
+    });
+
+    it('should provide an ability to update password', () => {
+      settingsPage.typePassword(newUser.newPassword);
+      settingsPage.clickUpdateBtn();
+      cy.visit('/settings');
+      cy.getByDataCy('logout-btn').click();
+      signInPage.visit();
+      signInPage.typeEmail(user.email);
+      signInPage.typePassword(newUser.newPassword);
+      signInPage.clickSignInBtn();
+      homePage.assertHeaderContainUsername(user.username);
+    });
+
+    it('should provide an ability to log out', () => {
+      cy.getByDataCy('logout-btn').click();
+      homePage.visit();
+      cy.getByDataCy('profile-link').should('not.exist');
+    });
   });
-
-  it('should provide an ability to update bio', () => {
-
-  });
-
-  it('should provide an ability to update an email', () => {
-
-  });
-
-  it('should provide an ability to update password', () => {
-
-  });
-
-  it('should provide an ability to log out', () => {
-
-  });
-});
