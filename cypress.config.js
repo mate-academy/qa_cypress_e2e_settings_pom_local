72ef3f92900e54a37af6114431799fbf2f7c1736
Dariusz Bydolek <darek021982@gmail.com>
2023-12-13 15:18:52 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from "cypress";
-const faker = require("faker");
-const { clear } = require("./dataBase");
+import { defineConfig } from 'cypress';
+const faker = require('faker');
+const { clear } = require('./dataBase');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      on("task", {
+      on('task', {
         generateUser() {
-          let randomNumber = Math.ceil(Math.random(1000) * 1000);
-          let userName = faker.name.firstName() + `${randomNumber}`
+          let randomNumber = Math.floor(Math.random(1000) * 1000);
+          let userName = faker.name.firstName() + `${randomNumber}`;
           return {
             username: userName.toLowerCase(),
             email: 'test'+`${randomNumber}`+'@mail.com',
@@ -18,10 +20,10 @@ module.exports = defineConfig({
         },
         generateArticle() {
           return {
-            title: faker.lorem.word(),
-            description: faker.lorem.words(),
-            body: faker.lorem.words(),
-            tag: faker.lorem.word()
+            title: faker.random.word(),
+            description: faker.random.words(),
+            body: faker.random.words(),
+            tag: faker.random.word()
           };;
         },
         'db:clear'() {
