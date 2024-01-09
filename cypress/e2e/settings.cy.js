5fd150fbcc0306f02affc4e80da1c3bfcbdd4663
SerhiiSoloviov <sergei.solovyov0294@gmail.com>
2024-01-09 15:11:32 +0200

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,60 @@
-/// <reference types="cypress" />
-/// <reference types="../support" />
+/// <reference types='cypress' />
+/// <reference types='../support' />
+
+import SettingsPageObject from '../support/pages/settings.pageObject';
 
 describe('Settings page', () => {
-  before(() => {
+  const settingsPage = new SettingsPageObject();
 
+  let user;
+  const newUserName = 'testuser';
+  const newUserBio = 'testbio';
+  const newUserEmail = 'test@mail.com';
+  const newUserPassword = 'test1234';
+
+  before(() => {
+    cy.task('db:clear');
+    
   });
 
   beforeEach(() => {
-
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.register(user.email, user.username, user.password); 
+      cy.login(user.email, user.password);
+    });
+    
+    settingsPage.visit();
+    
   });
 
-  it('should provide an ability to update username', () => {
-
+  it('should update username', () => {
+    settingsPage.typeUsername(newUserName);
+    settingsPage.clickUpdateBtn();
+    cy.reload();
+    settingsPage.usernameField.should('have.value', newUserName);
   });
 
-  it('should provide an ability to update bio', () => {
-
+  it('should update bio', () => {
+    settingsPage.typeBio(newUserBio);
+    settingsPage.clickUpdateBtn();
+    cy.reload();
+    settingsPage.bioField.should('have.value', newUserBio);
   });
 
-  it('should provide an ability to update an email', () => {
-
+  it('should update an email', () => {
+    settingsPage.typeEmail(newUserEmail);
+    settingsPage.clickUpdateBtn();
+    cy.reload();
+    settingsPage.emailField.should('have.value', newUserEmail);
   });
 
-  it('should provide an ability to update password', () => {
-
-  });
-
-  it('should provide an ability to log out', () => {
-
+  it('should update password', () => {
+    settingsPage.typePassword(newUserPassword);
+    settingsPage.clickUpdateBtn();
+    cy.clearAllCookies();
+    cy.login(user.email,user.username,newUserPassword);
+    settingsPage.visit();
+    settingsPage.emailField.should('have.value', user.email);
   });
 });
