6d158d3a7598b42c911a43cfd58c62abfbca8321
Rogers1275 <sashakondyuk@gmail.com>
2024-01-09 15:38:26 +0200

    added tests with POM for Conduit settings page

--- cypress.config.js
+++ cypress.config.js
@@ -10,10 +10,16 @@ module.exports = defineConfig({
         generateUser() {
           let randomNumber = Math.ceil(Math.random(1000) * 1000);
           let userName = faker.name.firstName() + `${randomNumber}`
+          let newUsername = faker.name.firstName() + `${randomNumber}`
+          let bio = faker.lorem.words();
           return {
             username: userName.toLowerCase(),
             email: 'test'+`${randomNumber}`+'@mail.com',
             password: '12345Qwert!',
+            newUsername: newUsername.toLowerCase(),
+            bio: bio,
+            newEmail: 'testnewEmail'+`${randomNumber}`+'@mail.com',
+            newPassword: 'Qwert!54321',
           };
         },
         generateArticle() {
--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,62 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
+
+import { SettingsPageObject } from "../support/pages/settings.pageObject";
+
+
 describe('Settings page', () => {
+  let user;
+  const settingsPage = new SettingsPageObject();
   before(() => {
 
   });
 
   beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
 
   });
 
   it('should provide an ability to update username', () => {
-
+    cy.login(user.email, user.username, user.password);
+    settingsPage.visit();
+    settingsPage.typeNewUsername(user.newUsername);
+    settingsPage.clickOnSubmitButton();
+    settingsPage.usernameField.should('have.value', user.newUsername);
   });
 
   it('should provide an ability to update bio', () => {
-
+    cy.login(user.email, user.username, user.password);
+    settingsPage.visit();
+    settingsPage.typeBio(user.bio);
+    settingsPage.clickOnSubmitButton();
+    settingsPage.bioField.should('have.value', user.bio);
   });
 
   it('should provide an ability to update an email', () => {
-
+    cy.login(user.email, user.username, user.password);
+    settingsPage.visit();
+    settingsPage.typeNewEmail(user.newEmail);
+    settingsPage.clickOnSubmitButton();
+    settingsPage.emailField.should('have.value', user.newEmail);
   });
 
   it('should provide an ability to update password', () => {
-
+    cy.login(user.email, user.username, user.password);
+    settingsPage.visit();
+    settingsPage.typeNewPassword(user.newPassword);
+    settingsPage.clickOnSubmitButton();
+    settingsPage.passwordField.should('have.value', user.newPassword);
   });
 
   it('should provide an ability to log out', () => {
-
+    cy.login(user.email, user.username, user.password);
+    settingsPage.visit();
+    settingsPage.clickOnLogoutButton();
+    cy.url().should('not.include', '/settings');
+    settingsPage.assertUserLoggedOut();
   });
 });
