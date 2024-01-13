b28ca10c7581a0368625ce16d810467989031bbd
NataliaLa <natalia.la1107@gmail.com>
2024-01-14 00:31:49 +0200

    Solution local conduit

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,65 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+const faker = require('faker');
 
+
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import ProfilePageObject from '../support/pages/profile.pageObject';
+
+const settingsPage = new SettingsPageObject();
+const profilePage = new ProfilePageObject();
+
+describe('Settings page', () => {
+
+  let user;
+
+  const newUserName = faker.name.firstName().toLowerCase();
+  const newUserBio = faker.random.word().toLowerCase();
+  const newUserEmail = `${newUserName}`+'@gmail.com';
+  const newUserPassword = 'blablaBLA123!';
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
   beforeEach(() => {
-
+    cy.login(user.email, user.password);
+    settingsPage.visit();
   });
 
   it('should provide an ability to update username', () => {
-
+    cy.log('Updating username...');
+    settingsPage.fillEmptyUsernameField(newUserName);
+    settingsPage.clickOnButton('update-settings-button');
+    settingsPage.assertUpdUsername(newUserName);
+    cy.log('Username updated successfully.');
   });
+  
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.fillBioField(newUserBio);
+    settingsPage.clickOnButton('update-settings-button');
+    profilePage.assertUserInfo(newUserBio);
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.fillEmptyEmailField(newUserEmail);
+    settingsPage.clickOnButton('update-settings-button');
+    settingsPage.assertUpdEmail(newUserEmail);
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.fillPasswordField(newUserPassword);
+    settingsPage.clickOnButton('update-settings-button');
+    settingsPage.assertUpdPassword(newUserPassword);
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.clickOnButton('logout-button');
+    settingsPage.checkUrlEndPoint('http://localhost:3000/');
   });
 });
---
+++ cypress/support/pages/settings.pageObject.js
@@ -0,0 +1,80 @@
+import PageObject from '../PageObject';
+
+class SettingsPageObject extends PageObject {
+url = '/settings';
+
+get profilePictureField() {
+  return cy.getByDataCy('profile-picture-field');
+}
+
+get usernameField() {
+  return cy.getByDataCy('username-field');
+}
+
+get bioField() {
+  return cy.getByDataCy('bio-field');
+}
+
+get emailField() {
+  return cy.getByDataCy('email-field');
+}
+
+get passwordField() {
+  return cy.getByDataCy('password-field');
+}
+
+get profileLink() {
+  return cy.getByDataCy('profile-link');
+}
+ get settingsLink() {
+  return cy.getByDataCy('settings-link');
+ }
+
+ goToSettings(link) {
+  this.settingsLink.type(link).click();
+ }
+
+fillAvatarField(url) {
+  this.profilePictureField.type(url);
+}
+
+fillEmptyUsernameField(username) {
+  this.usernameField.clear();
+  this.usernameField.type(username);
+}
+
+fillBioField(bio) {
+  this.bioField.type(bio);
+}
+
+fillEmptyEmailField(email) {
+  this.emailField.clear({ force: true });
+  this.emailField.type(email);
+}
+
+fillPasswordField(password) {
+  this.passwordField.type(password);
+}
+
+clickOnButton(buttonName) {
+  cy.getByDataCy(buttonName).click();
+}
+
+assertUpdUsername(newUsername) {
+  this.profileLink.should('contain.text', newUsername);
+}
+
+assertUpdEmail(newUserEmail) {
+  this.emailField.should('have.value', newUserEmail);
+}
+
+assertUpdPassword(newUserPassword) {
+  this.passwordField.should('have.value', newUserPassword);
+}
+
+checkUrlEndPoint(urlEndpoint) {
+  cy.url().should('include', urlEndpoint);
+}
+
+}
+export default SettingsPageObject;
