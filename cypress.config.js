cb8adeca4b33e79711d770aaf40085be8b29b016
MaksymKonosh <konoshm058@gmail.com>
2024-01-14 18:39:20 +0200

    Task is Done episode 1: The Phantom Menace

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "cypress";
-const faker = require("faker");
-const { clear } = require("./dataBase");
+import { defineConfig } from 'cypress';
+const faker = require('faker');
+const { clear } = require('./dataBase');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      on("task", {
+      on('task', {
         generateUser() {
           let randomNumber = Math.ceil(Math.random(1000) * 1000);
-          let userName = faker.name.firstName() + `${randomNumber}`
+          let userName = faker.name.firstName() + `${randomNumber}`;
           return {
             username: userName.toLowerCase(),
             email: 'test'+`${randomNumber}`+'@mail.com',
@@ -22,6 +22,14 @@ module.exports = defineConfig({
             description: faker.lorem.words(),
             body: faker.lorem.words(),
             tag: faker.lorem.word()
+          };;
+        },
+        generateUpdateData() {
+          let randomNumber = Math.ceil(Math.random(1000) * 1000);
+          return {
+            bio: faker.lorem.words(),
+            email: 'test'+`${randomNumber}`+'.updated'+'@mail.com',
+            password: '12345Qwert!updated'
           };;
         },
         'db:clear'() {
