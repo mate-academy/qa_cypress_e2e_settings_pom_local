32bebc42a9ce0e2cd0266e3aa2ece94cf4cb034e
Barbara Krysiak <b.krysiakk@gmail.com>
2024-02-01 16:35:47 +0100

    solution

--- cypress/support/pages/signIn.pageObject.js
+++ cypress/support/pages/signIn.pageObject.js
@@ -1,31 +1,82 @@
-import PageObject from '../PageObject';
+class SettingsPage {
+  url ='/settings';
 
-class SignInPageObject extends PageObject {
-  url = '/user/login';
-
-  get emailField() {
-    return cy.getByDataCy('email-sign-in');
+  get userImageField(){
+      return cy.get('[data-cy="url-of-profile-picture"]');
   }
 
-  get passwordField() {
-    return cy.getByDataCy('password-sign-in');
+  get usernameField(){
+      return cy.get('[data-cy="username-field"]');
   }
 
-  get signInBtn() {
-    return cy.getByDataCy('sign-in-btn');
+  get userBioField(){
+      return cy.get('[data-cy="user-bio-field"]');
   }
 
-  typeEmail(email) {
-    this.emailField.type(email);
+  get emailField(){
+      return cy.get('[data-cy="email-field"]');
   }
 
-  typePassword(password) {
-    this.passwordField.type(password);
+  get newPasswordField(){
+      return cy.get('[data-cy="new-password-field"]');
   }
 
-  clickSignInBtn() {
-    this.signInBtn.click();
+  get updatesettingsBtn(){
+      return cy.get('[type="submit"]'); 
   }
+
+  get logOutBtn(){
+      return cy.get('[class="btn btn-outline-danger"]');
+  }
+
+  visitPage(){
+      cy.visit(this.url);
+  }
+
+  urlIsSettings(){
+      cy.url().should('be', this.url);
+  }
+
+  fillUserImageField(imageUrl){
+      this.userImageField.type(imageUrl);
+  }
+
+  fillUsernameField(username){
+      this.usernameField.type(username);
+  }
+
+  clearUsernameField(){
+      this.usernameField.clear();
+  }
+
+  fillUserBioField(bio){
+      this.userBioField.type(bio);
+  }
+
+  fillEmailField(email){
+      this.emailField.type(email);
+  }
+
+  clearEmailField(){
+      this.emailField.clear();
+  }
+
+  fillNewPasswordField(password){
+      this.newPasswordField.type(password);
+  }
+
+  clickOnUpdateSettingsBtn(){
+      this.updatesettingsBtn.click();
+  }
+
+  clickOnLogOutBtn(){
+      this.logOutBtn.click();
+  }
+
+  assertUpdatedEmail(email){
+      this.emailField.should('have.value', email);
+  }
+
 }
 
-export default SignInPageObject;
+export default SettingsPage;
