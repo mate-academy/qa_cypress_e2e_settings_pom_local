9150b229f554d5e6ea602ed6d6df405a00cedb5a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-01 15:05:02 +0200

    signUp.cy.js

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,13 +1,30 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
+import SignUpPageObject from '../support/pages/SignUpPageObject';
+import { faker } from '@faker-js/faker';
+
+const signUpPage = new SignUpPageObject();
+
 describe('Sign Up page', () => {
-
-  before(() => {
-
+  beforeEach(() => {
+    cy.task('db:clear');
+    signUpPage.visit();
   });
 
-  it('should ...', () => {
+  it('should allow a user to sign up', () => {
+    const user = {
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
 
+    signUpPage.typeUsername(user.username);
+    signUpPage.typeEmail(user.email);
+    signUpPage.typePassword(user.password);
+    signUpPage.clickSignUpButton();
+
+    signUpPage.assertSuccessfulSignUp();
   });
+
 });
