541df812973805dcca5e61343a543cbada1d19ed
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-01 14:58:54 +0200

    article.cy.js

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,24 +1,47 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Article', () => {
-  before(() => {
+import ArticlePageObject from '../support/pages/ArticlePageObject';
+import { faker } from '@faker-js/faker';
 
+const articlePage = new ArticlePageObject();
+
+describe('Article', () => {
+  let user;
+  let article;
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+      cy.register(user.email, user.username, user.password);
+    });
   });
 
   beforeEach(() => {
-    cy.task('db:clear');
+    cy.signIn(user.email, user.password);
+    article = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraphs(2)
+    };
+    articlePage.visit();
   });
 
   it('should be created using New Article form', () => {
-
+    articlePage.createArticle(article.title, article.description, article.body);
   });
 
   it('should be edited using Edit button', () => {
-
+    const newArticle = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraphs(2)
+    };
+    articlePage.editArticle(newArticle.title, newArticle.description, newArticle.body);
   });
 
   it('should be deleted using Delete button', () => {
-
+    articlePage.deleteArticle();
   });
 });
