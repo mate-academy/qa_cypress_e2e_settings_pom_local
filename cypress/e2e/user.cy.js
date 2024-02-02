7b6f193589b660bc617bde54bdfd1e4133c4288f
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-02 13:36:52 +0200

    user.cy.js

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -3,23 +3,25 @@
 
 import UserPageObject from '../support/pages/UserPageObject';
 
-const userPage = new UserPageObject();
+describe('User actions', () => {
+  const userPage = new UserPageObject();
 
-describe('Follow/unfollow button', () => {
   beforeEach(() => {
-    cy.task('db:clear');
-    cy.createUserAndSignIn();
-
-    userPage.visit('/user-profile');
+    cy.login(); // Припускаємо, що у вас є команда для входу користувача
+    cy.visit('/user-profile'); // Змініть на актуальний URL сторінки профілю користувача
   });
 
-  it('should provide an ability to follow another user', () => {
-    userPage.followUser();
-    userPage.assertFollowed();
+  it('should allow a user to follow another user', () => {
+    userPage.clickFollow();
+
+    // Тут має бути перевірка на успішну підписку, наприклад, перевірка тексту кнопки на "Unfollow"
   });
 
-  it('should provide an ability to unfollow the followed user', () => {
-    userPage.unfollowUser();
-    userPage.assertUnfollowed();
+  it('should allow a user to unfollow another user', () => {
+    userPage.clickUnfollow();
+
+    // Тут має бути перевірка на успішну відписку, наприклад, перевірка тексту кнопки на "Follow"
   });
+
+  // Додайте інші тестові випадки за потреби
 });
