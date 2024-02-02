3366cd2b829631f5f8663561275dc0f966c1e778
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-02 13:30:55 +0200

    settings.cy.js

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -4,44 +4,31 @@
 import SettingsPageObject from '../support/pages/SettingsPageObject';
 import { faker } from '@faker-js/faker';
 
-const settingsPage = new SettingsPageObject();
-
-describe('Settings page', () => {
-  let user;
-
-  before(() => {
-    cy.task('db:clear');
-    cy.task('generateUser').then((generatedUser) => {
-      user = generatedUser;
-      cy.register(user.email, user.username, user.password);
-    });
-  });
+describe('Settings', () => {
+  const settingsPage = new SettingsPageObject();
 
   beforeEach(() => {
-    cy.signIn(user.email, user.password);
-    settingsPage.visit();
+    cy.login(); // Припускаємо, що у вас є команда для входу користувача
+    cy.visit('/settings'); // Змініть на актуальний URL сторінки налаштувань
   });
 
-  it('should provide an ability to update username', () => {
+  it('should provide the ability to update Username', () => {
     const newUsername = faker.internet.userName();
-    settingsPage.updateUsername(newUsername);
+    settingsPage.typeUsername(newUsername);
+    settingsPage.submit();
+
+    cy.get('.success-message').should('contain', 'Your settings have been successfully updated');
+    settingsPage.usernameField.should('have.value', newUsername);
   });
 
-  it('should provide an ability to update bio', () => {
-    const newBio = faker.lorem.sentence();
-    settingsPage.updateBio(newBio);
-  });
-
-  it('should provide an ability to update an email', () => {
+  it('should provide the ability to update Email', () => {
     const newEmail = faker.internet.email();
-    settingsPage.updateEmail(newEmail);
+    settingsPage.typeEmail(newEmail);
+    settingsPage.submit();
+
+    cy.get('.success-message').should('contain', 'Your settings have been successfully updated');
+    settingsPage.emailField.should('have.value', newEmail);
   });
 
-  it('should provide an ability to update password', () => {
-    const newPassword = faker.internet.password();
-    settingsPage.updatePassword(newPassword);
-  });
-
-  it('should provide an ability to log out', () => {
-  });
+  // Додайте інші тестові випадки за потреби
 });
