37472f2539673ff80fe34da6346c911cd0eb637d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-03 13:58:56 +0200

    SettingsPageObject.js

--- cypress/support/pages/SettingsPageObject.js
+++ cypress/support/pages/SettingsPageObject.js
@@ -1,6 +1,15 @@
 import PageObject from './PageObject';
 
-class SettingsPageObject {
+class SettingsPageObject extends PageObject {
+  constructor() {
+    super();
+    this.url = '/settings';
+  }
+
+  get bioField() {
+    return cy.get('[data-cy="bio-field"]');
+  }
+
   get usernameField() {
     return cy.get('[data-cy="username-field"]');
   }
@@ -9,29 +18,39 @@ class SettingsPageObject {
     return cy.get('[data-cy="email-field"]');
   }
 
-  get newPasswordField() {
-    return cy.get('[data-cy="new-password-field"]');
+  get passwordField() {
+    return cy.get('[data-cy="password-field"]');
   }
 
-  get submitButton() {
-    return cy.get('[data-cy="submit-btn"]');
+  get updateButton() {
+    return cy.get('[data-cy="update-button"]');
+  }
+
+  typeBio(bio) {
+    this.bioField.clear().type(bio);
+    return this;
   }
 
   typeUsername(username) {
     this.usernameField.clear().type(username);
+    return this;
   }
 
   typeEmail(email) {
     this.emailField.clear().type(email);
+    return this;
   }
 
-  typeNewPassword(newPassword) {
-    this.newPasswordField.clear().type(newPassword);
+  typePassword(password) {
+    this.passwordField.clear().type(password);
+    return this;
   }
 
-  submit() {
-    this.submitButton.click();
+  clickUpdate() {
+    this.updateButton.click();
+    return this;
   }
+
 }
 
 export default SettingsPageObject;
