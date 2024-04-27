0dd798a3f41ea338f15501a12517fd7370c743b2
Tykhon Kozachenko <tykhonkozachenko@gmail.com>
2024-04-27 13:09:32 +0200

    e2e_testing

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,20 @@ import { clear } from './dataBase';
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
           let randomNumber = Math.ceil(Math.random(1000) * 1000);
-          let userName = faker.name.firstName() + `${randomNumber}`;
+          let userName = faker.person.firstName() + `${randomNumber}`;
+
           return {
             username: userName.toLowerCase(),
             email: 'test'+`${randomNumber}`+'@mail.com',
             password: '12345Qwert!',
           };
         },
+
         generateArticle() {
           return {
             title: faker.lorem.word(),
@@ -24,6 +27,7 @@ module.exports = defineConfig({
             tag: faker.lorem.word()
           };;
         },
+
         'db:clear'() {
           clear();
           return null;
--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,118 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+import SignInPageObject from '../support/pages/signIn.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import ProfilePageObject from '../support/pages/profile.pageObject';
 
+const signInPage = new SignInPageObject();
+const homePage = new HomePageObject();
+const settingsPage = new SettingsPageObject();
+const profilePage = new ProfilePageObject();
+
+describe('Settings page', () => {
+  let user;
+  let newUser;
+
+  before(() => {
+    cy.task('generateUser')
+      .then((generatedUser1) => {
+        newUser = generatedUser1;
+      });
+
+    cy.task('generateUser')
+      .then((generatedUser2) => {
+        user = generatedUser2;
+      });
   });
 
   beforeEach(() => {
+    cy.task('db:clear');
 
+    cy.login(
+      user.email, 
+      user.username, 
+      user.password
+    );
+
+    settingsPage.visit();
   });
 
   it('should provide an ability to update username', () => {
+    settingsPage
+      .typeUserName(newUser.username);
 
+    settingsPage
+      .clickUpdateBtn();
+
+    settingsPage
+      .clickOkBtn();
+
+    homePage
+      .assertHeaderContainUsername(newUser.username);
   });
 
   it('should provide an ability to update bio', () => {
+    settingsPage
+      .typeBio(newUser.username);
 
+    settingsPage
+      .clickUpdateBtn();
+
+    settingsPage
+      .clickOkBtn();
+
+    homePage.usernameLink
+      .click();
+
+    profilePage
+      .assertBio(newUser.username);
   });
 
   it('should provide an ability to update an email', () => {
+    settingsPage
+      .typeEmail(newUser.email);
 
+    settingsPage
+      .clickUpdateBtn();
+
+    settingsPage
+      .clickOkBtn();
+
+    settingsPage
+      .clickLogoutBtn();
+
+    cy.login(newUser.email, user.password);
+
+    homePage
+      .assertHeaderContainUsername(user.username);
   });
 
   it('should provide an ability to update password', () => {
+    settingsPage
+      .typeEmail(newUser.password);
 
+    settingsPage
+      .clickUpdateBtn();
+
+    settingsPage
+      .clickOkBtn();
+
+    settingsPage
+      .clickLogoutBtn();
+    
+    cy.login(user.email, newUser.password);
+
+    homePage
+      .assertHeaderContainUsername(user.username);
   });
 
   it('should provide an ability to log out', () => {
+    settingsPage
+      .clickLogoutBtn();
 
+    homePage
+      .checkNotAuthorized();
   });
 });
