a9a3bebe8da5308fba8eb5508da648b99254c581
Ihor Sokolenko <igorsokolenko1986@gmail.com>
2024-05-22 18:48:21 +0300

    Conduit POM

---
+++ cypress/support/pages/userProfile.pageObject.js
@@ -0,0 +1,58 @@
+import PageObject from '../PageObject';
+
+class UserProfilePage extends PageObject {
+
+  visit(user) {
+    cy.visit(`/profile/${user}`);
+  }
+  
+  getUsernameInput() {
+    return cy.get('[data-cy=Username]');
+  }
+
+  getBioInput() {
+    return cy.get('[data-cy=bio]');
+  }
+
+  getEmailInput() {
+    return cy.get('[data-cy=email]');
+  }
+
+  getPasswordInput() {
+    return cy.get('[data-cy=password]');
+  }
+
+  getUpdateButton() {
+    return cy.get('[data-cy=update-button]');
+  }
+
+  updateUsername(username) {
+    this.getUsernameInput().clear().type(username);
+    this.getUpdateButton().click();
+  }
+
+  updateBio(bio) {
+    this.getBioInput().clear().type(bio);
+    this.getUpdateButton().click();
+  }
+
+  updateEmail(email) {
+    this.getEmailInput().clear().type(email);
+    this.getUpdateButton().click();
+  }
+
+  updatePassword(password) {
+    this.getPasswordInput().clear().type(password);
+    this.getUpdateButton().click();
+  }
+  get bio(){
+    return cy.get('[data-cy=bio]');
+  }
+
+  assertUpdatedBio(bio) {
+   this.bio.should('contain', bio);
+  }
+
+}
+
+export default UserProfilePage;
