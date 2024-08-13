41205d446da87dbcfbc245c886a4ec2eaf1e28f8
Olha Panchenko <panchenko-85@ukr.net>
2024-08-13 12:43:16 +0300

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -17,27 +17,36 @@ describe('Settings page', () => {
   });
 
   it('should provide an ability to update username', () => {
-    settingsPage.usernameField.type('new');
+    settingsPage.fillTheUsernameField('new');
     settingsPage.updateBtn.click();
     settingsPage.usernameField.should('have.value', user.username + 'new');
   });
 
   it('should provide an ability to update bio', () => {
-    settingsPage.bioField.type('new');
+    settingsPage.fillTheBioField('new');
     settingsPage.updateBtn.click();
     settingsPage.bioField.should('have.value', 'new');
   });
 
   it('should provide an ability to update an email', () => {
-    settingsPage.emailField.clear().type('qqiot10@qa.team');
+    settingsPage.fillTheEmailField('qqiot10@qa.team');
     settingsPage.updateBtn.click();
     settingsPage.emailField.should('have.value', 'qqiot10@qa.team');
+
+    // Логін з новим email
+    cy.reload().clearCookies();
+    cy.visit('/user/login');
+    cy.getByDataCy('email-sign-in').type('qqiot10@qa.team');
+    cy.getByDataCy('password-sign-in').type(user.password);
+    cy.getByDataCy('sign-in-btn').click();
+    cy.getByDataCy('profile-link').should('contain', user.username);
   });
 
   it('should provide an ability to update password', () => {
     const newPass = 'qqiot10@qa!';
-    settingsPage.passwordField.type(newPass);
+    settingsPage.fillThePasswordField(newPass);
     settingsPage.updateBtn.click();
+    
     cy.reload().clearCookies();
     cy.visit('/user/login');
     cy.getByDataCy('email-sign-in').type(user.email);
