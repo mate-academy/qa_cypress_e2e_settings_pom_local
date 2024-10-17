8d8f0a5474ce2e099ac7aca5f32d64b3d5c913ce
Oleg <_email@example.com>
2024-10-17 14:57:40 +0300

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -2,12 +2,17 @@
 /// <reference types="../support" />
 
 describe('Sign Up page', () => {
-
   before(() => {
-
+    cy.visit('/register'); // Visit the registration page
   });
 
-  it('should ...', () => {
+  it('should create a new account', () => {
+    cy.get('[data-cy=username]').type('NewUser');
+    cy.get('[data-cy=email]').type('newuser@example.com');
+    cy.get('[data-cy=password]').type('NewPassword123');
+    cy.get('[data-cy=signup-submit]').click();
 
+    // Verify successful registration
+    cy.contains('Your Feed').should('be.visible');
   });
 });
