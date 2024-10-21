66b7f9329e2f8d61737dd6933b932fdb2d021814
Anton Zhyhanovskyi <zhyganovskiy7@gmail.com>
2024-10-21 14:51:43 +0300

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -2,31 +2,47 @@
 /// <reference types="../support" />
 
 describe('Settings page', () => {
+  let user;
   before(() => {
-
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.visit('/');
+    cy.register();
+    cy.login();
+    settingsPage.visit();
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.typeUsername(user.username);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage(user.username);
   });
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.typeBio(user.bio);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.typeEmail(user.email);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.typePassword(user.password);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.logout();
+    cy.url().should('include', '/');
   });
 });
