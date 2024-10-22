2c8a14f7b3d7a416d26fadf39c3f350c8653bf3c
mhyryak <hyryakm@gmail.com>
2024-10-22 21:54:30 +0200

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,48 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import homePageObject from '../support/pages/home.pageObject';
+import accountPageObject from '../support/pages/account.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject';
+
+const settingsPage = newSettingsPageObject();
+const homePage = newhomePageObject();
+const accountPage = newaccountPageObject();
+const signInPage = newSignInPageObject();
+
 describe('Settings page', () => {
-  before(() => {
-
-  });
-
-  beforeEach(() => {
-
-  });
-
-  it('should provide an ability to update username', () => {
-
+  let user;
+    settingsPage.visit();
+    settingsPage.typeUsername(user2.username);
+    settingsPage.clickUpdateSettingsBtn();
+    homePage.assertHeaderContainUsername(user2.username);
   });
 
   it('should provide an ability to update bio', () => {
-
-  });
-
-  it('should provide an ability to update an email', () => {
+  describe('Settings page', () => {
+    signInPage.typeEmail(user2.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+    homePage.assertHeaderContainUsername(user2.email);
 
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.visit();
+    settingsPage.typePassword(user2.password);
+    settingsPage.clickUpdateSettingsBtn();
+    settingsPage.visit();
+    settingsPage.clickLogoutBtn();
+    signInPage.visit();
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user2.password);
+    signInPage.clickSignInBtn();
+    homePage.assertHeaderContainUsername(user.username);
   });
-
-  it('should provide an ability to log out', () => {
-
+  it.only('should provide an ability to log out', () => {
+    settingsPage.visit();
+    settingsPage.clickLogoutBtn();
+    homePage.usernameLink.should('not.exist');
   });
-});
+  });
