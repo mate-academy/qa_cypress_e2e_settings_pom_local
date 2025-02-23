d0d152f79205bf6894b66c3934c0c65b963c239a
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-02-23 02:45:27 +0100

    add

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,41 @@
+// cypress/e2e/settings.spec.js
+
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
+import SettingsPage from '../support/pages/settings.pageObject';
+
+const settingsPage = new SettingsPage();
+
 describe('Settings page', () => {
-  before(() => {
-
-  });
-
   beforeEach(() => {
-
+    cy.task('db:clear'); // Clear database before each test
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.visit();
+    settingsPage.updateUsername('new_username');
+    // Add assertions as needed
   });
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.visit();
+    settingsPage.updateBio('New bio text');
+    // Add assertions as needed
   });
 
-  it('should provide an ability to update an email', () => {
-
+  it('should provide an ability to update email', () => {
+    settingsPage.visit();
+    settingsPage.updateEmail('newemail@example.com');
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.visit();
+    settingsPage.updatePassword('new_password');
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.visit();
+    settingsPage.logout();
   });
 });
